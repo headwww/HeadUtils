be0c17ea4216929ef8ab21cc18ae024e72a483d0
shuwen <1243889238@qq.com>
2021-04-06 10:37:39 +0800

    工具包

--- toolkit/src/main/java/com/head/toolkit/DeviceUtils.java
+++ toolkit/src/main/java/com/head/toolkit/DeviceUtils.java
@@ -1,12 +1,16 @@
 package com.head.toolkit;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
 public class DeviceUtils {
     private DeviceUtils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
     }
 
     /**
-     * 返回版本
+     *  获取设备系统版本号
      * @return
      */
     public static String getSDKVersionName() {
@@ -14,12 +18,50 @@ public class DeviceUtils {
     }
 
     /**
-     * 返回版本编码
-     *
+     * 获取设备系统版本码
      * @return
      */
     public static int getSDKVersionCode() {
         return android.os.Build.VERSION.SDK_INT;
     }
 
+    /**
+     * 获取本地软件版本号
+     */
+    public static int getLocalVersion(Context ctx)
+    {
+        int localVersion = 0;
+        try
+        {
+            PackageInfo packageInfo = ctx.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(ctx.getPackageName(), 0);
+            localVersion = packageInfo.versionCode;
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return localVersion;
+    }
+
+    /**
+     * 获取本地软件版本号名称
+     */
+    public static String getLocalVersionName(Context ctx)
+    {
+        String localVersion = "";
+        try
+        {
+            PackageInfo packageInfo = ctx.getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(ctx.getPackageName(), 0);
+            localVersion = packageInfo.versionName;
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return localVersion;
+    }
 }
